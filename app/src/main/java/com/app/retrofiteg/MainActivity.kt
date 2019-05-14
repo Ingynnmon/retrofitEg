2ef9c8440118dc80166698d24bd957b663b99e6c
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-05-14 11:38:39 +0630

    modified

--- app/src/main/java/com/app/retrofiteg/MainActivity.kt
+++ app/src/main/java/com/app/retrofiteg/MainActivity.kt
@@ -2,11 +2,34 @@ package com.app.retrofiteg
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        final EditText titleEt = (EditText) findViewById(R.id.et_title);
+        final EditText bodyEt = (EditText) findViewById(R.id.et_body);
+        Button submitBtn = (Button) findViewById(R.id.btn_submit);
+        mResponseTv = (TextView) findViewById(R.id.tv_response);
+
+        mAPIService = ApiUtils.getAPIService();
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title = titleEt.getText().toString().trim();
+                String body = bodyEt.getText().toString().trim();
+                if(!TextUtils.isEmpty(title) && !TextUtils.isEmpty(body)) {
+                    sendPost(title, body);
+                }
+            }
+        });
     }
 }
